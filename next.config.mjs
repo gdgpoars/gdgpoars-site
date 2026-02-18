eaf4ae600c44077591ff5c452a1800981fa9d583
gdgpoars <gdgpoars@gmail.com>
2026-02-18 14:06:29 -0300

    Ajustes

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,28 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default nextConfig
